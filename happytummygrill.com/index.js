c0b7440715d817741cfa6254d50a150236c5d5a8
Alisa Song <alisasong@gmail.com>
2017-07-23 23:08:23 -0700

    Update menu buttons

--- happytummygrill.com/index.js
+++ happytummygrill.com/index.js
@@ -2,10 +2,29 @@
 
     'use strict';
 
+    var menuButtons = [
+        'menu-teriyaki-combinations',
+        'menu-yakisoba-fried-rice',
+        'menu-stir-fry-chinese-express',
+        'menu-noodle-soup-salads',
+        'menu-side-orders'
+    ];
+
+    var selectedMenuButtonIndex = 0;
+
     window.addEventListener('load', function () {
-        window.addEventListener('scroll', function () {
-            console.log('test');
-        });
+        for (var i = 0; i < menuButtons.length; i++) {
+            document.getElementById(menuButtons[i]).addEventListener('click', clickMenuButton);
+        }
     });
 
+    function clickMenuButton() {
+        var clickedId = this.id;
+        var selectedMenuButton = document.getElementById(menuButtons[selectedMenuButtonIndex]);
+        if (clickedId != selectedMenuButton.id) {
+            selectedMenuButton.classList.remove('menu-btn-selected');
+            this.classList.add('menu-btn-selected');
+            selectedMenuButtonIndex = menuButtons.indexOf(clickedId);
+        }
+    }
 })();
